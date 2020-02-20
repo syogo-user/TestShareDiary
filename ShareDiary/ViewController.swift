2f2056c037ce8ccc51a866ccde61f5c176fc56b7
syogo-user <s.photo.trumpet.005@gmail.com>
2020-02-21 01:12:17 +0900

    Initial Commit

---
+++ ShareDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShareDiary
+//
+//  Created by 小野寺祥吾 on 2020/02/21.
+//  Copyright © 2020 syogo-user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
