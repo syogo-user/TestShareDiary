23a2ddf9a272a26443e48b511b6b51ec93eb2a7b
syogo-user <s.photo.trumpet.005@gmail.com>
2020-02-22 15:09:47 +0900

    firebaseインストールまで完了

---
+++ ShareDiary/FriendListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FriendListViewController.swift
+//  ShareDiary
+//
+//  Created by 小野寺祥吾 on 2020/02/21.
+//  Copyright © 2020 syogo-user. All rights reserved.
+//
+
+import UIKit
+
+class FriendListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
